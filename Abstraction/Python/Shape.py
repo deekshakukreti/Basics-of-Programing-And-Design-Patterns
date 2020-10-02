84299ae5e29e49e1813679d691f5ff72fc55ebc4
DEEKSHA KUKRETI <56849867+deekshakukreti@users.noreply.github.com>
2020-10-02 14:42:52 +0100

    Update Shape.py

--- Abstraction/Python/Shape.py
+++ Abstraction/Python/Shape.py
@@ -1,1 +1,15 @@
 
+from abc import ABC, abstractmethod
+ 
+class Shape(ABC):
+    
+    @abstractmethod
+    def GetNumberofCorners(self):
+        pass
+        
+class Pentagon(Shape):
+    def GetNumberofCorners(self):
+        print("Pentagon has five corners.")
+        
+x = Pentagon()
+x.GetNumberofCorners()
