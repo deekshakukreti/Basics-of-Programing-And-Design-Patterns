0f8bf82823a02d48aaf104659fa32a549ecfbc6e
DEEKSHA KUKRETI <deeksha.kukreti18@gmail.com>
2020-10-09 14:53:52 +0100

    SPRImplementation

---
+++ SOLID Principles/Single Responsibility Principle/ImplementSPR/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IssueStackOverflow
+{
+    public partial class Form1 : Form
+    {
+        SportsCar theCar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            theCar.Accelerate(Int32.Parse(textBox2.Text));
+            updateUI();
+        }
+
+        private void updateUI()
+        {
+            txtMaxSPeed.Text = theCar.Speed.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            theCar = new SportsCar(comboBox1.Text);
+            txtMaxSPeed.Text = theCar.MaxSpeed.ToString();
+            lblstatus.Text = theCar.Status.ToString();
+        }
+
+
+    }
+}
---
+++ SOLID Principles/Single Responsibility Principle/ImplementSPR/SportsCar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueStackOverflow
+{
+    class SportsCar : interfaceISportCar
+    {
+        public string Brand { get; }
+        public int Speed { get; set; }
+        public int MaxSpeed { get; }
+        public string Status { get; }
+
+        
+        public virtual bool Accelerate(int SpeedIncr)
+        {
+            bool speedExceed = Speed + SpeedIncr > MaxSpeed;
+
+            Speed = (speedExceed) ? Speed : Speed + SpeedIncr;
+
+            return speedExceed;            
+        }
+
+        string[] availableBrands = new string[] { "Ferrari", "Mercedes", "BMW" };
+
+        public SportsCar(string brand)
+        {
+            if (!availableBrands.Contains(brand)) return;
+            else Brand = brand;
+
+            switch(brand)
+            {
+                case "Ferrari":
+                    MaxSpeed = 350;
+                    Status = "It is Ferrari";
+                    break;
+                case "Mercedes":
+                    MaxSpeed = 300;
+                    Status = "It is Mercedes";
+                    break;
+                case "BMW":
+                    MaxSpeed = 250;
+                    Status = "It is BMW";
+                    break;
+            }
+        }        
+    }
+}
