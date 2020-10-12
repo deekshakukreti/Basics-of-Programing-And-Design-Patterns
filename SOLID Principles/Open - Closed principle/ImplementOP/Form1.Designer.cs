6766d51cf8af569afccfa09390c88400a76492a4
DEEKSHA KUKRETI <deeksha.kukreti18@gmail.com>
2020-10-12 21:04:25 +0100

    OCPrinciple Completion

---
+++ SOLID Principles/Open - Closed principle/ImplementOP/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace IssueStackOverflow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtMaxSPeed = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = ">>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pick Up a Brand";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Max Speed";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ferrari",
+            "Mercedes",
+            "BMW"});
+            this.comboBox1.Location = new System.Drawing.Point(31, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(170, 28);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtMaxSPeed
+            // 
+            this.txtMaxSPeed.Location = new System.Drawing.Point(31, 153);
+            this.txtMaxSPeed.Name = "txtMaxSPeed";
+            this.txtMaxSPeed.Size = new System.Drawing.Size(170, 26);
+            this.txtMaxSPeed.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 221);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(324, 153);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(97, 44);
+            this.btnexit.TabIndex = 8;
+            this.btnexit.Text = "EXIT";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblstatus.ForeColor = System.Drawing.Color.Red;
+            this.lblstatus.Location = new System.Drawing.Point(319, 74);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(144, 25);
+            this.lblstatus.TabIndex = 9;
+            this.lblstatus.Text = "Status : None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 269);
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtMaxSPeed);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtMaxSPeed;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Label lblstatus;
+    }
+}
+
--- SOLID Principles/Open - Closed principle/README.md
+++ SOLID Principles/Open - Closed principle/README.md
@@ -1,8 +1,7 @@
 
 # Open - Closed Principle
 
-Bertrand Mayer in 1988 paraphrased that software entities should be open for extension,
- but closed for modification.
+Bertrand Mayer in 1988 paraphrased that software entities should be open for extension, but closed for modification.
 
 What is that Open - Closed Principle brings to us ? When a single change in program results
 in cascade of changes to dependent modules, the program becomes fragile, rigid, unpredictable and
@@ -12,6 +11,27 @@ works. Modules in Open - Closed principle are open for extension and closed for 
 
 Module that cannot be changed is considered to have a fixed behaviour, however the module that is changed
 is extended with new funcionality. The question is how can these two opposing attributes be implemented?
+It is possible by abstraction as they are abstract base classes. It is easily possible to make changes to
+abstract classes. For example, in the below figure both client and server classes are not abstract. If the client
+class want to use different server class, then the client class will be changed to name the new server class.
+
+![cp](https://github.com/deekshakukreti/Images/blob/main/cp.png)
+
+However, in below figure the ServerAbstract class is an abstract class with virtual member functions. The client
+class uses this abstraction. The object of client class will be using the object of the derivative of the server class.
+A new serverAbstract class can be created, if we want to use object of the new server class. The client class remain
+unchanged.
+
+![op](https://github.com/deekshakukreti/Images/blob/main/op.png)
+
+
+
+
+
+
+
+
+
 
 
 
